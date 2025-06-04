c2ab611d8f66ee555d8c96ae82d980b08d1f2993
LucaBla <me@lucablazevic.dev>
2025-06-04 17:13:19 +0200

    update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,6 @@
 #Redis.current = Redis.new(url:  ENV['REDIS_URL'],
 #  port: ENV['REDIS_PORT'],
 #  db:   ENV['REDIS_DB'])
+require 'redis'
+
 $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
