67a991d11d3fce676bb43b0a1297338c8ea237d4
LucaBla <me@lucablazevic.dev>
2025-06-04 17:41:20 +0200

    change cable.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,14 +45,6 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
-	config.action_cable.redis_connector = ->(_config) {
-	  Redis.new(
-		url: ENV['REDIS_URL'],
-		ssl: true,
-		ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-	  )
-	}
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
